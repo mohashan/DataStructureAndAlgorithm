efe20f3a8eea755a870a323645eff253071f5e4b
Mohashan <msh200x@gmail.com>
2023-11-15 19:10:34 +0330

    Merge Sort Implemented

---
+++ Sort/Sort/MergeSort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort;
+
+public class MergeSort
+{
+    public int[] DoSort(int[] List)
+    {
+        if (List.Length <= 1)
+            return List;
+
+        int MiddleIndex = List.Length / 2;
+
+        var List1 = DoSort(List[..MiddleIndex]);
+        var List2 = DoSort(List[MiddleIndex..]);
+
+        int RightIndex = 0, LeftIndex = 0;
+
+        for(int i=0; i < List.Length; i++)
+        {
+            if (LeftIndex == -1)
+            {
+                List[i] = List2[RightIndex];
+                RightIndex++;
+                if (RightIndex > (List2.Length - 1))
+                    RightIndex = -1;
+                continue;
+            }
+
+            if(RightIndex == -1)
+            {
+                List[i] = List1[LeftIndex];
+                LeftIndex++;
+                if (LeftIndex > (List1.Length - 1))
+                    LeftIndex = -1;
+                continue;
+            }
+
+            if (List1[LeftIndex] <= List2[RightIndex])
+            {
+                List[i] = List1[LeftIndex];
+                LeftIndex++;
+                if (LeftIndex > (List1.Length - 1))
+                    LeftIndex = -1;
+                continue;
+            }
+            else
+            {
+                List[i] = List2[RightIndex];
+                RightIndex++;
+                if (RightIndex > (List2.Length - 1))
+                    RightIndex = -1;
+                continue;
+            }
+        }
+
+        return List;
+    }
+}
--- Sort/Sort/Program.cs
+++ Sort/Sort/Program.cs
@@ -3,6 +3,7 @@ var bubbleSort = new BubbleSort();
 var insertionSort = new InsertionSort();
 var selectionSort = new SelectionSort();
 var quickSort = new QuickSort();
+var mergeSort = new MergeSort();
 
 int[] list = { 23, 12, 48, 1, 25, 14, 56, 19, 12, 41, 31, 28 };
 
@@ -11,7 +12,8 @@ Console.WriteLine($"Unsorted Array : {list.IntArray2String()}");
 Console.WriteLine($"Bubble Sort : {bubbleSort.DoSort(list.OrderBy(c => Guid.NewGuid()).ToArray()).IntArray2String()}");
 Console.WriteLine($"Insertion Sort : {insertionSort.DoSort(list.OrderBy(c => Guid.NewGuid()).ToArray()).IntArray2String()}");
 Console.WriteLine($"Selection Sort : {selectionSort.DoSort(list.OrderBy(c => Guid.NewGuid()).ToArray()).IntArray2String()}");
-list = new int[] { 19, 12, 12, 14, 1, 31, 25, 23, 48, 41, 56, 28 };
-Console.WriteLine($"Quick Sort : {quickSort.DoSort(list).IntArray2String()}");
+//list = new int[] { 19, 12, 12, 14, 1, 31, 25, 23, 48, 41, 56, 28 };
+Console.WriteLine($"Quick Sort : {quickSort.DoSort(list.OrderBy(c => Guid.NewGuid()).ToArray()).IntArray2String()}");
+Console.WriteLine($"Merge Sort : {mergeSort.DoSort(list.OrderBy(c => Guid.NewGuid()).ToArray()).IntArray2String()}");
 
 Console.ReadLine();
